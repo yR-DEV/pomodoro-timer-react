72093fcd88e94603b86e624843f7bf743e49eeb8
yeah-right <claar.zack@gmail.com>
2019-05-14 11:11:07 -0600

    component created inside of components folder, wired up to app.js and being rendered properly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Timer from './components/Timer';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app-main">
         <h1>P O M O T I M 3 R</h1>
+        <Timer />
       </div>
     );
   }
